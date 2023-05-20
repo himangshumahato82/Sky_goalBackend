2726e1d39aef4c7627925c93dacc0b40a234d884
himangshumahato82 <himangshumahato82@gmail.com>
2023-05-20 10:19:41 +0530

    done

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-  
+dotenv.config();
+const MONGO_URL=process.env.MONGO_URL
 const ConnectDB = () => {
-  const mognURL =process.env.mognURL
+  
   mongoose
-    .connect(mognURL, { useNewUrlParser: true })
+    .connect(MONGO_URL, { useNewUrlParser: true })
     .then((res) => console.log(res.connection.db.databaseName))
     .catch((error) => console.log(error));
 };
